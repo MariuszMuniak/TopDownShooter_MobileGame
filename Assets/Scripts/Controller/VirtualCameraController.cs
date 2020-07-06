b9fab2d8a21f07a8d21d6aabef9af5f768a8a918
MariuszMuniak <geldern1989@gmail.com>
2020-07-06 17:33:48 +0200

    Configured Virtual Camera

---
+++ Assets/Scripts/Controller/VirtualCameraController.cs
@@ -0,0 +1,26 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS_MG.Controller
+{
+    public class VirtualCameraController : MonoBehaviour
+    {
+        CinemachineVirtualCamera virtualCamera;
+        Transform player;
+
+        private void Awake()
+        {
+            virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+            player = GameObject.FindWithTag("Player").transform;
+        }
+        private void Start()
+        {
+            if (virtualCamera != null && player != null)
+            {
+                virtualCamera.LookAt = player;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS_MG.Core
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        Transform player = null;
+
+        private void Awake()
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+
+        private void LateUpdate()
+        {
+            if (player == null) { return; }
+
+            transform.position = player.position;
+        }
+    }
+}

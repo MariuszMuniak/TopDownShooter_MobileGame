2825344ec256d084b00b581d9bdbb670534fee42
MariuszMuniak <geldern1989@gmail.com>
2020-07-13 14:53:07 +0200

    Created basic EnemyController script

---
+++ Assets/Scripts/Control/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TDS_MG.Movement;
+using UnityEngine;
+
+namespace TDS_MG.Control
+{
+    public class EnemyController : MonoBehaviour
+    {
+        EnemyMover mover;
+        Transform player;
+
+        private void Awake()
+        {
+            mover = GetComponent<EnemyMover>();
+            player = GameObject.FindWithTag("Player").transform;
+        }
+
+        private void Update()
+        {
+            mover.MoveTo(player.position);
+        }
+    }
+}

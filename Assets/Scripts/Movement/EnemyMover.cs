edc330706ab6a95f60c1358aa5a4d2beed075aa5
MariuszMuniak <geldern1989@gmail.com>
2020-07-13 14:52:56 +0200

    Created basic EnemyMover script

---
+++ Assets/Scripts/Movement/EnemyMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TDS_MG.Movement
+{
+    public class EnemyMover : MonoBehaviour
+    {
+        NavMeshAgent agent;
+        Animator animator;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 position)
+        {
+            agent.SetDestination(position);
+        }
+
+        private void UpdateAnimator()
+        {
+            float speed = agent.velocity.magnitude;
+            animator.SetFloat("speed", speed);
+        }
+    }
+}

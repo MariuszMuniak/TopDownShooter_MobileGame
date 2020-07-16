396f92cce7b642d3c29d65dcba1e7bfa72c9a1f1
MariuszMuniak <geldern1989@gmail.com>
2020-07-16 14:15:49 +0200

    Added muti spawn points to EnemyGenerator

--- Assets/Scripts/Core/EnemyGenerator.cs
+++ Assets/Scripts/Core/EnemyGenerator.cs
@@ -6,7 +6,8 @@ namespace TDS_MG.Core
 {
     public class EnemyGenerator : MonoBehaviour
     {
-        [SerializeField] float spawnAreaRadius = 1f;
+        [SerializeField] SpawnPoint[] spawnPoints = new SpawnPoint[0];
+        [Space]
         [SerializeField] GameObject zombie = null;
         [SerializeField] Wave[] waves = new Wave[1];
 
@@ -50,10 +51,11 @@ namespace TDS_MG.Core
         {
             Wave wave = waves[waveIndex];
             currentWave = wave;
+            SpawnPoint spawnPoint = RandomSpawnPoint();
 
             for (int i = 0; i < currentWave.zombieNumber; i++)
             {
-                Instantiate(zombie, RandomWorldPositionInSpawnArea(), transform.rotation);
+                Instantiate(zombie, RandomWorldPositionInSpawnArea(spawnPoint), transform.rotation);
             }
 
             timeToNextWave = wave.timeToNextWave;
@@ -62,21 +64,29 @@ namespace TDS_MG.Core
             NextWaveIndex();
         }
 
-        private Vector3 RandomWorldPositionInSpawnArea()
+        private Vector3 RandomWorldPositionInSpawnArea(SpawnPoint spawnPoint)
         {
             Vector3 position;
+            float radius = spawnPoint.areaRadius;
 
             do
             {
                 position = new Vector3
                 {
-                    x = Random.Range(-spawnAreaRadius, spawnAreaRadius),
+                    x = Random.Range(-radius, radius),
                     y = 0f,
-                    z = Random.Range(-spawnAreaRadius, spawnAreaRadius)
+                    z = Random.Range(-radius, radius)
                 };
-            } while (position.magnitude > spawnAreaRadius);
+            } while (position.magnitude > radius);
 
-            return transform.TransformPoint(position);
+            return spawnPoint.center.TransformPoint(position);
+        }
+
+        private SpawnPoint RandomSpawnPoint()
+        {
+            SpawnPoint spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+            return spawnPoint;
         }
 
         private void NextWaveIndex()
@@ -96,10 +106,24 @@ namespace TDS_MG.Core
             public float timeToNextWave;
         }
 
+        [System.Serializable]
+        private class SpawnPoint
+        {
+            public Transform center = null;
+            public float areaRadius = 1f;
+        }
+
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(transform.position, spawnAreaRadius);
+
+            foreach (SpawnPoint spawnPoint in spawnPoints)
+            {
+                if (spawnPoint.center != null)
+                {
+                    Gizmos.DrawWireSphere(spawnPoint.center.position, spawnPoint.areaRadius);
+                }
+            }
         }
     }
 }

06c7489e43915dd84be8103275dbc9145198272e
MariuszMuniak <geldern1989@gmail.com>
2020-09-03 09:43:07 +0200

    Edited weapon upgrade layout grid

---
+++ Assets/Scripts/Attributes/WeaponStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+namespace TDS_MG.Attributes
+{
+    [System.Serializable]
+    public class WeaponStats
+    {
+        public int damage = 0;
+        public int fireRate = 0;
+        public float reloadSpeed = 0f;
+    } 
+}

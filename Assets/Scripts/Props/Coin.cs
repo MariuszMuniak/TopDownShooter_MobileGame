ad1d6bf40f53192608379025058275023ff4d691
MariuszMuniak <geldern1989@gmail.com>
2020-09-23 12:12:09 +0200

    Created Coin prefab with explosion effect

---
+++ Assets/Scripts/Props/Coin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TDS_MG.Shop;
+using UnityEngine;
+
+namespace TDS_MG.Props
+{
+    public class Coin : MonoBehaviour
+    {
+        [SerializeField] int minValue = 10;
+        [SerializeField] int maxValue = 100;
+        [SerializeField] float rotationSpeed = 1f;
+        [SerializeField] Transform coinModel = null;
+        [SerializeField] GameObject pickUpEffect = null;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                InstantiatePickUpEffekt();
+                GivePlayerRandomValue(other);
+                Destroy(gameObject);
+            }
+        }
+
+        private void GivePlayerRandomValue(Collider other)
+        {
+            Wallet wallet = other.GetComponent<Wallet>();
+            int randomValue = Random.Range(minValue, maxValue + 1);
+            wallet.AddMoney(randomValue);
+        }
+
+        private void InstantiatePickUpEffekt()
+        {
+            if (pickUpEffect != null)
+            {
+                GameObject instantiatedEffect =  Instantiate(pickUpEffect, coinModel.position, Quaternion.identity);
+                Destroy(instantiatedEffect, 5f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Props/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS_MG.Props
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] Transform target = null;
+        [SerializeField] float rotationSpeed = 1f;
+        [SerializeField] bool x = false;
+        [SerializeField] bool y = false;
+        [SerializeField] bool z = false;
+
+        private void LateUpdate()
+        {
+            Vector3 eulers = new Vector3
+            {
+                x = x ? 1f : 0f,
+                y = y ? 1f : 0f,
+                z = z ? 1f : 0f,
+            };
+
+            target.Rotate(eulers, rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+}

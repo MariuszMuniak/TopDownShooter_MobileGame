203ceb2c1e8c901321e41f49e42b50d4a207a19e
MariuszMuniak <geldern1989@gmail.com>
2020-09-29 11:35:36 +0200

    Added sound to buttons
    Clicking on button in UI play sound.

---
+++ Assets/Scripts/Audio/Audio2DPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS_MG.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Audio2DPlayer : MonoBehaviour
+    {
+        AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Start()
+        {
+            audioSource.spatialBlend = 0f;
+        }
+
+        public void PlayOneShot()
+        {
+            if (audioSource.clip != null)
+            {
+                audioSource.PlayOneShot(audioSource.clip);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Play2DSoundOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TDS_MG.Audio;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TDS_MG.UI
+{
+    public class Play2DSoundOnClick : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Audio2DPlayer audio2DPlayer = FindObjectsOfType<Audio2DPlayer>()[0];
+            GetComponent<Button>().onClick.AddListener(() => audio2DPlayer.PlayOneShot());
+        }
+    }
+}

a6d6fab040ea136f7bc54173e5a1f5c3db26d251
MariuszMuniak <geldern1989@gmail.com>
2020-09-30 11:47:19 +0200

    Added health bar to zombie

---
+++ Assets/Scripts/Core/FacingToCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS_MG.Core
+{
+    public class FacingToCamera : MonoBehaviour
+    {
+        Camera mainCamera;
+
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            transform.LookAt(mainCamera.transform);
+        }
+    }
+}

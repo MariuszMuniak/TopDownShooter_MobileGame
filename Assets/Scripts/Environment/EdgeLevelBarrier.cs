6e89f508708a22649a12afc634997244f7b0338d
MariuszMuniak <geldern1989@gmail.com>
2020-10-06 13:27:39 +0200

    Created Level_02

---
+++ Assets/Scripts/Environment/EdgeLevelBarrier.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS_MG.Environment
+{
+    public class EdgeLevelBarrier : MonoBehaviour
+    {
+        void Start()
+        {
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+            if (meshRenderer != null)
+            {
+                meshRenderer.enabled = false;
+            }
+        }
+    }
+}

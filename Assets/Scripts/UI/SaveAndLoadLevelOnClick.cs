383935001b208ba8adf7d1855fa088f62f52cc83
MariuszMuniak <geldern1989@gmail.com>
2020-10-29 12:38:20 +0100

    Created SaveAndLoadLevelOnClick script

---
+++ Assets/Scripts/UI/SaveAndLoadLevelOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TDS_MG.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TDS_MG.UI
+{
+    public class SaveAndLoadLevelOnClick : MonoBehaviour
+    {
+        [SerializeField] int sceneBuildIndex = 0;
+
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(() => FindObjectOfType<SavingWrapper>().SaveAndLoadScene(sceneBuildIndex));
+        }
+    }
+}

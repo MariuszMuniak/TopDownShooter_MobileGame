0dacdb2ed36316605133d06d5a28d0b8b61f30e6
MariuszMuniak <geldern1989@gmail.com>
2020-10-29 12:38:36 +0100

    Created SaveAndLoadNextLevelOnClick.n script

---
+++ Assets/Scripts/UI/SaveAndLoadNextLevelOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TDS_MG.Control;
+using TDS_MG.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TDS_MG.UI
+{
+    public class SaveAndLoadNextLevelOnClick : MonoBehaviour
+    {
+        void Start()
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            nextSceneIndex = nextSceneIndex == FindObjectOfType<LevelController>().GetMaxSceneBuildIndex() ? 0 : nextSceneIndex;
+
+            GetComponent<Button>().onClick.AddListener(() => FindObjectOfType<SavingWrapper>().SaveAndLoadScene(nextSceneIndex));
+        }
+    } 
+}

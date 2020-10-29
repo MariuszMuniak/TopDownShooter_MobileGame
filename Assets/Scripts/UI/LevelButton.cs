053975ef6c1c675683345b837832f0d78128d585
MariuszMuniak <geldern1989@gmail.com>
2020-10-29 12:37:50 +0100

    Created LevelButton script

---
+++ Assets/Scripts/UI/LevelButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TDS_MG.Control;
+using TDS_MG.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TDS_MG.UI
+{
+    public class LevelButton : MonoBehaviour
+    {
+        [SerializeField] int sceneBuildIndex = 0;
+
+        Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        void Start()
+        {
+            button.interactable = FindObjectOfType<LevelController>().IsLevelComplished(sceneBuildIndex);
+            button.onClick.AddListener(() => FindObjectOfType<SavingWrapper>().SaveAndLoadScene(sceneBuildIndex));
+        }
+    } 
+}
